a172b320507406442d2332189d36c2361e7d4a7a
Sedata14 <132104929+Sedata14@users.noreply.github.com>
2025-11-24 14:01:40 +0100

    vrai changement du ddc

---
+++ server/src/text.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2/promise');
+
+async function testConnection() {
+  try {
+    const connection = await mysql.createConnection({
+      host: '127.0.0.1',      // localhost
+      user: 'root',           // ton user MySQL
+      password: 'root',    // mot de passe
+      database: 'gamedatadb'  // <--- ton schema
+    });
+
+    console.log('✅ Connexion OK !');
+    const [rows] = await connection.query('SHOW TABLES;');
+    console.log('Tables :', rows);
+
+    await connection.end();
+  } catch (err) {
+    console.error('❌ Erreur de connexion :', err.message);
+  }
+}
+
+testConnection();
+console.log('Fin du script');
