52f8bec912478d816882a4eb52c2e7ab0e3134ce
Sedata14 <132104929+Sedata14@users.noreply.github.com>
2025-11-26 09:33:59 +0100

    Correction front register/login

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -16,7 +16,7 @@ export default function Register() {
     const data = { username, email, password };
 
     try {
-      const res = await fetch('http://localhost:5000/api/register', {
+      const res = await fetch('http://localhost:5000/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data),
--- server/server.js
+++ server/server.js
@@ -12,8 +12,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // --- MIDDLEWARES ---
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:5173", // ton front
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true
+}));app.use(express.json());
 
 // --- ROUTES ---
 app.use('/auth', authRoutes);
